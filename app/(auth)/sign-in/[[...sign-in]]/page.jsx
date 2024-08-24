e68ffa0e3bcde9c6f835a33c64228b57e7432058
anex45 <148676435+anex45@users.noreply.github.com>
2024-08-24 22:26:53 +0530

    Update page.jsx

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -42,8 +42,8 @@ export default function Page() {
             </h2>
 
             <p class="mt-4 leading-relaxed text-white/90">
-             <br> Sample Sign_in:-</br>
-             <br> Username:- samplesignin30@gmail.com<br>
+             <br> Sample Sign_in:-<br>
+              Username:- samplesignin30@gmail.com<br>
               password:- samplesignin30
             </p>
           </div>
